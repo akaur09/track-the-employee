17febafef65f3f2a5be93b4dae69e5959026a6c6
akaur091 <amniderkaur.09@gmail.com>
2022-05-02 22:26:34 -0700

    add function to view employees

--- server.js
+++ server.js
@@ -71,5 +71,20 @@ function firstPrompt() {
 function viewEmployee(){
     console.log("Viewing Employees");
 
+    var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+         ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+        ON m.id = e.manager_id`
     
+    connection.query(query, function (err,res){
+        if (err) throw err;
+        console.log(res);
+        console.log("Employees checked");
+        firstPrompt();
+    });
 }
+
